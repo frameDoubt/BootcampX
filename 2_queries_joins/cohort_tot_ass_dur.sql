a6f9b9d7b97817f8a817940f3f7f07c1b834cb0b
Brandon Wright <brandon.w.wright4@gmail.com>
2021-02-03 23:04:39 +0000

    add query that gets the total number of assignments for days

--- 2_queries_joins/cohort_tot_ass_dur.sql
+++ 2_queries_joins/cohort_tot_ass_dur.sql
@@ -2,4 +2,4 @@ SELECT SUM(assignment_submissions.duration) AS total_duration
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'FEB12'
+WHERE cohorts.name = 'FEB12';

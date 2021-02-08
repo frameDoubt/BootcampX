851847cf4375b99a4ce19d1edbffac6790de5e5f
Brandon Wright <brandon.w.wright4@gmail.com>
2021-02-08 06:24:28 +0000

    add user input sanitization

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,15 @@
 const { Pool } = require('pg');
 
+const queryStr = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+const userInput = [`%${process.argv[2] || 'JUL02'}%`];
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,17 +17,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
+pool.query(queryStr, userInput)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    })
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
